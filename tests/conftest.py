b6504c4315796987120ff24b574414eef06cadb4
Koray <KorayKirli@gmail.com>
2024-08-24 22:19:00 +0200

    add tests for encrypt_upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import os  # noqa: D100
 from shutil import copyfile
 
 import pytest
+from grz_upload.encrypt_upload import prepare_c4gh_keys, prepare_header
 
 config_path = "tests/mock_files/mock_config.yaml"
 input_path = "tests/mock_files/mock_input_file.txt"
@@ -17,8 +18,7 @@ def datadir(tmpdir_factory):
     return datadir
 
 
-def copy_file_to_tempdir(input_path, datadir):  # noqa: F821
-    print(datadir)
+def copy_file_to_tempdir(input_path, datadir):
     filename = os.path.basename(input_path)
     target = datadir.join(filename)
     copyfile(input_path, target)
@@ -27,20 +27,37 @@ def copy_file_to_tempdir(input_path, datadir):  # noqa: F821
 
 @pytest.fixture
 def temp_log_file(datadir):
-    print(datadir)
     log_file = datadir.join("log.txt")
     return str(log_file)
 
 
 @pytest.fixture
 def temp_input_file(datadir):
-    print(datadir)
     return copy_file_to_tempdir(input_path, datadir)
+
+
+def create_large_file(input_file, output_file, target_size):
+    # Read the content of the original file
+    with open(input_file, encoding='utf8') as infile:
+        content = infile.read()
+    # Initialize the size of the new file and open it for writing
+    current_size = 0
+    with open(output_file, 'w') as outfile:
+        while current_size < target_size:
+            outfile.write(content)
+            current_size += len(content)
+    return output_file
+
+
+@pytest.fixture
+def temp_large_input_file(datadir):
+    temp_large_input_file_path = datadir.join('temp_large_input_file.txt')
+    target_size = 1024 * 1024 * 6  # create 5MB file, multiupload limit is 5MB
+    return create_large_file(input_path, temp_large_input_file_path, target_size)
 
 
 @pytest.fixture
 def temp_private_key_file( datadir):
-    print(datadir)
     return copy_file_to_tempdir(private_key_path, datadir)
 
 
@@ -55,9 +72,7 @@ test_file,replace_dir/mock_input_file.txt"""
 
 @pytest.fixture
 def temp_metadata_file(datadir):
-    print(datadir)
     metadata_file = datadir.join("mock_metadata.csv")
-    print(metadata_file)
     with open(metadata_file, 'w') as f:
         f.write(metadata_content.replace("replace_dir", str(datadir)))
     return str(metadata_file)
@@ -65,7 +80,7 @@ def temp_metadata_file(datadir):
 
 config_content = """metadata_file_path: 'replace_dir/mock_metadata.csv'
 public_key_path: 'replace_dir/mock_public_key.pub'
-s3_url: 'testing'
+s3_url: ''
 s3_bucket: 'testing'
 s3_access_key: 'testing'
 s3_secret: 'testing'"""
@@ -73,9 +88,20 @@ s3_secret: 'testing'"""
 
 @pytest.fixture
 def temp_config_file(datadir):
-    print(datadir)
     config_file = datadir.join("config.yaml")
-    print(config_file)
     with open(config_file, 'w') as f:
         f.write(config_content.replace("replace_dir", str(datadir)))
     return str(config_file)
+
+
+@pytest.fixture
+def temp_c4gh_keys(temp_public_key_file):
+    keys = prepare_c4gh_keys(temp_public_key_file)
+    return keys
+
+
+@pytest.fixture
+def temp_c4gh_header(temp_c4gh_keys):
+    header_pack = prepare_header(temp_c4gh_keys)
+    return header_pack
+
--- tests/test_encrypt_upload.py
+++ tests/test_encrypt_upload.py
@@ -1,14 +1,17 @@
-import os  # noqa: D100
+import hashlib  # noqa: D100
+import os
 import shutil
-from unittest import mock
+import subprocess
+from math import ceil
 
-import pytest
+import boto3
+import yaml
 from grz_upload.encrypt_upload import (
+    SEGMENT_SIZE,
     calculate_md5,
     encrypt_part,
     encrypt_segment,
     log_progress,
-    main,
     prepare_c4gh_keys,
     prepare_header,
     print_summary,
@@ -17,13 +20,10 @@ from grz_upload.encrypt_upload import (
     validate_metadata,
 )
 from moto import mock_aws
-from nacl.public import PrivateKey
 
 # create fixtures from the mock files stored under tests
 # some files get modified, use temdir for storing these temporary copies
 
-def test_config_file(temp_config_file):
-    print(temp_config_file)
 
 def test_log_progress(temp_log_file):
     """Test for log_progress function"""
@@ -44,57 +44,114 @@ def test_validate_metadata(temp_metadata_file, temp_input_file):
     result = validate_metadata(temp_metadata_file)
     assert result==1 
 
-# def test_print_summary(temp_metadata_file, temp_log_file):
-#     print_summary(temp_metadata_file, temp_log_file)
-#     # Add your assertions based on expected summary output
+
+def test_print_summary(temp_metadata_file, temp_log_file, capsys):
+    expected_message = """Total files: 1\nUploaded files: 0\nFailed files: 0\nWaiting files: 1\n"""
+    print_summary(temp_metadata_file, temp_log_file)
+    captured = capsys.readouterr()
+    assert captured.out == expected_message
+    # Add your assertions based on expected summary output
 
 
 def test_calculate_md5(temp_input_file):
     md5 = calculate_md5(temp_input_file)
     assert isinstance(md5, str)
     assert len(md5) == 32  # MD5 hash is 32 characters long
+    assert md5 == "710781ec9efd25b87bfbf8d6cf4030e9"
 
 
-# def test_prepare_c4gh_keys():
-#     public_key = PrivateKey.generate().public_key
-#     keys = prepare_c4gh_keys(public_key)
-#     assert isinstance(keys, dict)
-#     assert "session_key" in keys
-#     assert "public_key" in keys
-#
-#
-# def test_prepare_header():
-#     keys = {'session_key': b'test_key', 'public_key': b'test_public_key'}
-#     header = prepare_header(keys)
-#     assert isinstance(header, bytes)
-#
-#
-# def test_encrypt_segment():
-#     data = b"test data"
-#     key = b"test_key_32_bytes_long____"
-#     encrypted_data = encrypt_segment(data, key)
-#     assert isinstance(encrypted_data, bytes)
-#     assert len(encrypted_data) > len(data)
-#
-#
-# def test_encrypt_part():
-#     byte_string = b"test bytes"
-#     session_key = b"test_key_32_bytes_long____"
-#     encrypted_part = encrypt_part(byte_string, session_key)
-#     assert isinstance(encrypted_part, bytes)
-#
-#
-# def test_stream_encrypt_and_upload(temp_data_file, mock_s3_client, temp_log_file):
-#     keys = {'session_key': b'test_key', 'public_key': b'test_public_key'}
-#     stream_encrypt_and_upload(temp_data_file, "file_id", keys, mock_s3_client, "test_bucket", temp_log_file)
-#     # Add assertions based on expected interactions with mock_s3_client
-#
-#
-# def test_main():
-#     config = {"key": "value"}  # Mock your config as needed
-#     with mock.patch("grz_upload.encrypt_upload.stream_encrypt_and_upload") as mock_upload:
-#         main(config)
-#         mock_upload.assert_called_once()
+def test_prepare_c4gh_keys(temp_public_key_file):
+    keys = prepare_c4gh_keys(temp_public_key_file)
+    # single key in tupple
+    assert len(keys) == 1
+    # key method is set to 0
+    assert keys[0][0] == 0
+    # private key is generated
+    assert len(keys[0][1]) == 32
+
+
+def test_prepare_header(temp_c4gh_keys):
+    header_pack = prepare_header(temp_c4gh_keys)
+    assert len(header_pack) == 3
+    # assert header size
+    assert len(header_pack[0]) == 124
+    # size of session key
+    assert len(header_pack[1]) == 32
+    # pass back the c4gh keys
+    assert header_pack[2] == temp_c4gh_keys
+
+
+def test_encrypt_segment(temp_c4gh_header):
+    data = b"test segment content"
+    key = temp_c4gh_header[1]
+    encrypted_data = encrypt_segment(data, key)
+    assert isinstance(encrypted_data, bytes)
+    # size is larger by 12 nonce and 16 mac(?)
+    assert len(encrypted_data) == len(data) + 12 + 16 
+
+
+def test_encrypt_part(temp_input_file, temp_c4gh_header):
+    with open(temp_input_file, 'rb') as infile:
+        byte_string = infile.read()
+        session_key = temp_c4gh_header[1]
+        encrypted_part = encrypt_part(byte_string, session_key)
+    print(len(encrypted_part))
+    file_size = len(byte_string)
+    segment_no = ceil(file_size/SEGMENT_SIZE)
+    encrypted_part_size = file_size + segment_no * (12 + 16)
+    assert encrypted_part_size == len(encrypted_part)
+
+@mock_aws
+def test_stream_encrypt_and_upload(temp_large_input_file,
+                                   temp_c4gh_keys,
+                                   temp_config_file,
+                                   temp_log_file,
+                                   datadir,
+                                   temp_private_key_file):
+    bucket_name = 'temp_bucket'
+    file_id = 'temp_id'
+    # create client
+    with open(temp_config_file, encoding="utf-8") as config_file:
+        config = yaml.safe_load(config_file)
+
+    # Initialize S3 client for uploading
+    temp_s3_client = boto3.client('s3',
+                             endpoint_url=None,
+                             aws_access_key_id=config['s3_access_key'],
+                             aws_secret_access_key=config['s3_secret'])
+    
+    # create bucket
+    temp_s3_client.create_bucket(Bucket=bucket_name)
+    # set chunks to 5MB
+    multipart_chunk_size = 1024 * 1024 * 5
+    # encrypt and upload
+    input_md5_st, enc_md5_st = stream_encrypt_and_upload(file_location=temp_large_input_file,
+                              file_id=file_id,
+                              keys=temp_c4gh_keys,
+                              s3_client=temp_s3_client,
+                              s3_bucket=bucket_name,
+                              log_file=temp_log_file,
+                              multipart_chunk_size=multipart_chunk_size
+                              )
+    temp_enc_file = datadir.join('temp_enc_file.txt.c4gh')
+    # download encrypted file
+    temp_s3_client.download_file(bucket_name, file_id, temp_enc_file)
+    # compare md5s
+    input_md5 = hashlib.md5(open(temp_large_input_file,'rb').read()).hexdigest()
+    enc_md5 = hashlib.md5(open(temp_enc_file,'rb').read()).hexdigest()
+    assert input_md5 == input_md5_st
+    assert enc_md5 == enc_md5_st
+    # Decrypt file and make sure content is correct
+    # Use subprocess - crypt4gh arguments < > are problematic for passing them directly
+    # Open the input and output files
+    temp_decr_file = datadir.join('temp_denc_file.txt')
+    with open(temp_enc_file, 'rb') as input_file, open(temp_decr_file, 'wb') as output_file:
+        command = ["crypt4gh", "decrypt", "--sk", temp_private_key_file]
+        # Run the command with input and output redirection
+        subprocess.run(command, stdin=input_file, stdout=output_file)
+    
+    denc_md5 = enc_md5 = hashlib.md5(open(temp_decr_file,'rb').read()).hexdigest()
+    assert denc_md5 == enc_md5
 
 
 def test_cleanup_time(datadir):
