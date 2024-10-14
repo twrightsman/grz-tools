0c7faa64c661e1a8037e6c90233aa4564634456a
Thomas Sell <thomas.sell@bih-charite.de>
2024-10-14 17:13:44 +0200

    add tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
-from grz_upload.parser import SubmissionMetadata
+from grz_upload.parser import EncryptedSubmission, SubmissionMetadata
+from pathlib import Path
 
 
 def test_SubmissionMetadata(temp_metadata_file_path):
@@ -8,6 +9,15 @@ def test_SubmissionMetadata(temp_metadata_file_path):
     assert errors == []
 
     assert len(submission_metadata.files) > 0
+
+def test_EncryptedSubmission():
+    input_path = "/submission/files/a.fastq"
+
+    for i in (input_path, Path(input_path)):
+        enc_path = EncryptedSubmission.get_encrypted_file_path(i)
+        assert enc_path == Path(input_path + ".c4gh")
+        enc_path = EncryptedSubmission.get_encryption_header_path(input_path)
+        assert enc_path == Path(input_path + ".c4gh_header")
 
 # TODO: test encrypt submission
 # TODO: test upload submission
