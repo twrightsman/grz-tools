a7a7e8e105c7eb2bb0d567b73bf4da76427fd4d3
Travis Wrightsman <travis.wrightsman@uni-tuebingen.de>
2025-06-27 15:08:43 +0200

    chore(grz-pydantic-models)!: drop deprecated functionality (#220)

--- packages/grz-pydantic-models/src/grz_pydantic_models/v1_1_1/metadata.py
+++
@@ -1,5 +0,0 @@
-from warnings import warn
-
-from grz_pydantic_models.submission.metadata.v1 import *  # noqa: F403
-
-warn("The v1_1_1.metadata package is deprecated. Please import submission.metadata.v1 instead.")
