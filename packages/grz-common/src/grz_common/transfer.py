a116499de19655ec9c4a43093c2c077dd10efbbc
Travis Wrightsman <travis.wrightsman@uni-tuebingen.de>
2025-07-03 11:40:26 +0200

    fix(grz-common,grzctl,grz-cli): remove runtime dependency on type stubs (#258)
    * fix(grz-common,grzctl,grz-cli): fix runtime dependency on type stubs
    
    We only need the types while checking, not at runtime.
    
    * Fix undefined objects when not typechecking
    
    * Pretend to be dependabot
    
    * grzctl too

--- packages/grz-common/src/grz_common/transfer.py
+++ packages/grz-common/src/grz_common/transfer.py
@@ -2,11 +2,19 @@
 Common methods for transferring data to and from GRZ buckets.
 """
 
+from typing import TYPE_CHECKING
+
 import boto3
 from boto3 import client as boto3_client  # type: ignore[import-untyped]
 from botocore.config import Config as Boto3Config  # type: ignore[import-untyped]
-from types_boto3_s3 import S3Client
-from types_boto3_s3.service_resource import S3ServiceResource
+
+if TYPE_CHECKING:
+    from types_boto3_s3 import S3Client
+    from types_boto3_s3.service_resource import S3ServiceResource
+else:
+    # avoid undefined objects when not type checking
+    S3Client = object
+    S3ServiceResource = object
 
 from .models.s3 import S3Options
 
